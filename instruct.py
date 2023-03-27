7fd9bb926d70c7670154b13c0eae4e121378191c
danimal141 <hideaki.ishii1204@gmail.com>
2023-03-27 19:01:20 +0900

    Fix format

--- instruct.py
+++ instruct.py
@@ -9,7 +9,7 @@ prompt = input("Input question: ")
 
 resp = openai.Completion.create(
     prompt=prompt,
-    model='text-davinci-003',  # InstructGPT
+    model="text-davinci-003",  # InstructGPT
     temperature=0.7,
     max_tokens=256,
     top_p=1,
