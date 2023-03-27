811b86bf189ed56ccad85b7f011993ef78cce4e3
danimal141 <hideaki.ishii1204@gmail.com>
2023-03-27 18:23:21 +0900

    Implement openai call

---
+++ instruct.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAPI_KEY")
+prompt = input("Input question: ")
+
+resp = openai.Completion.create(
+    prompt=prompt,
+    model='text-davinci-003',  # InstructGPT
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+print(resp["choices"][0]["text"])
